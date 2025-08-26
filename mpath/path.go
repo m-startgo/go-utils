d7b2780bded0fcb66a82357d5e6b0608d2a2d990
墨七 <meichangliang@outlook.com>
2025-08-26 17:03:24 +0800

    x

--- mpath/path.go
+++ mpath/path.go
@@ -2,21 +2,17 @@ package mpath
 
 import (
 	"os"
-	"runtime"
 )
 
 // Home 返回用户的主目录。
 // 它会尝试跨平台常用的环境变量。
 // 若均未设置，返回空字符串。
 func Home() string {
-	if runtime.GOOS == "windows" {
-		home := os.Getenv("HOMEDRIVE") + os.Getenv("HOMEPATH")
-		if home == "" {
-			home = os.Getenv("USERPROFILE")
-		}
-		return home
+	dir, err := os.UserHomeDir()
+	if err != nil {
+		return ""
 	}
-	return os.Getenv("HOME")
+	return dir
 }
 
 // Pwd 返回当前工作目录及可能发生的错误。
