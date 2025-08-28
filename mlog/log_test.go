04b31516ad49d5adb255fc737eaff7a29208d4af
墨七 <meichangliang@outlook.com>
2025-08-28 15:56:21 +0800

    logo封装完毕

--- mlog/log_test.go
+++ mlog/log_test.go
@@ -1,6 +1,10 @@
 package mlog
 
-import "testing"
+import (
+	"fmt"
+	"sync"
+	"testing"
+)
 
 var Log *Logger
 
@@ -10,10 +14,31 @@ func TestNewLog(t *testing.T) {
 		Path: "./mo7-logs",
 		Name: "m",
 	})
-	Log.Info("this is info")
-	Log.Warn("this is warn")
-	Log.Error("this is error")
-	Log.Debug("this is debug")
+	go Log.Info("this is info") // 并发写入测试，其实写日志建立 goroutine 开销可能更大
+
+	var wg sync.WaitGroup
+	wg.Add(4)
+
+	go func() {
+		defer wg.Done()
+		Log.Info("this is info")
+	}()
+	go func() {
+		defer wg.Done()
+		Log.Warn("this is warn")
+	}()
+	go func() {
+		defer wg.Done()
+		Log.Error("this is error")
+	}()
+	go func() {
+		defer wg.Done()
+		Log.Debug("this is debug")
+	}()
+
+	fmt.Println("等待所有任务完成...")
+	wg.Wait()
+	fmt.Println("所有任务完成。")
 }
 
 // go test -v -run TestClearLog
