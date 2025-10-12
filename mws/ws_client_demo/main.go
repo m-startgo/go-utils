7d9a0fcba2a1801bc1293ecf617cae476c833218
墨七 <meichangliang@outlook.com>
2025-10-12 18:34:02 +0800

    优化udp库

--- mws/ws_client_demo/main.go
+++ mws/ws_client_demo/main.go
@@ -2,11 +2,8 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"time"
 
 	"github.com/m-startgo/go-utils/mstr"
-	"github.com/m-startgo/go-utils/mws"
 )
 
 var (
@@ -15,24 +12,7 @@ var (
 )
 
 func main() {
-	// 注意：server 必须先启动
-	// server listens upgrade on path /ws, make sure client requests that path
 	wsUrl := mstr.Join("ws://", IP, ":", PORT, "/ws")
-	conn, _, err := mws.Dial(wsUrl)
-	if err != nil {
-		log.Fatal("dial error:", err)
-	}
-	conn.SetOnMessage(func(mt int, data []byte) {
-		fmt.Println("server reply:", string(data))
-	})
-	conn.SetOnClose(func(err error) {
-		fmt.Println("closed:", err)
-	})
 
-	_ = conn.SendText("hello from client")
-
-	time.Sleep(1 * time.Second)
-	_ = conn.Close()
-	// wait a bit to let close callbacks print
-	time.Sleep(200 * time.Millisecond)
+	fmt.Println("wsUrl:", wsUrl)
 }
--- mws/ws_server_demo/main.go
+++ mws/ws_server_demo/main.go
@@ -2,11 +2,8 @@ package main
 
 import (
 	"fmt"
-	"log"
 
 	"github.com/m-startgo/go-utils/mstr"
-	"github.com/m-startgo/go-utils/mws"
-	"github.com/valyala/fasthttp"
 )
 
 var (
@@ -15,32 +12,6 @@ var (
 )
 
 func main() {
-	h := func(ctx *fasthttp.RequestCtx) {
-		switch string(ctx.Path()) {
-		case "/ws":
-			err := mws.ServeUpgrade(ctx, func(c *mws.Conn) {
-				fmt.Println("client connected")
-				c.SetOnMessage(func(mt int, data []byte) {
-					fmt.Printf("recv: %s\n", string(data))
-					// echo back
-					_ = c.SendText("echo: " + string(data))
-				})
-				c.SetOnClose(func(err error) {
-					fmt.Println("closed:", err)
-				})
-			})
-			if err != nil {
-				log.Println("upgrade error:", err)
-			}
-		default:
-			ctx.SetStatusCode(404)
-		}
-	}
-
 	wsUrl := mstr.Join(IP, ":", PORT)
-	fmt.Println("ws server listening:", wsUrl)
-	err := fasthttp.ListenAndServe(wsUrl, h)
-	if err != nil {
-		log.Fatal(err)
-	}
+	fmt.Println("wsUrl:", wsUrl)
 }
