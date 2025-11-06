b31b69688e48ebf24dc1c6dc6bc5b8f7ad105c2d
墨七 <meichangliang@outlook.com>
2025-11-06 23:43:19 +0800

    更新依赖

--- script/clear_dir.ts
+++ script/clear_dir.ts
@@ -1,15 +1,15 @@
-import { $ } from 'bun';
+#!/usr/bin/env ts-node
+/* 
+清理目录 ，这个脚本会直接删除某些目录
+*/
+import shell from 'shelljs';
 
-const currentDir = process.cwd();
-console.info('当前执行命令的目录:', currentDir);
-$.cwd(currentDir);
-
-try {
-  await $`git clean -fdX`;
-} catch (error) {
-  console.error(`rm err code: ${error.exitCode}`);
-  console.info(error.stdout.toString());
-  console.info(error.stderr.toString());
-  process.exit(1);
+if (!shell.which('git')) {
+  shell.echo('Sorry, this script requires git');
+  shell.exit(1);
 }
+
+// 清理目录
+shell.exec('git clean -fdX');
+// 进程 结束
 process.exit(0);
